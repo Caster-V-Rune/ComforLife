0aa44c7a613acc2f2136e0e9662cecbe1479d535
Caster-V-Rune <caster.v.rune@gmail.com>
2016-03-01 16:45:05 +0800

    初始化ssh2环境
    初始化ssh2环境于idea
    by rune

---
+++ src/com/entity/UserEntity.java
@@ -0,0 +1,79 @@
+package com.entity;
+
+import javax.persistence.*;
+
+/**
+ * Created by rune on 3/1/16.
+ */
+@Entity
+@Table(name = "user", schema = "test", catalog = "")
+public class UserEntity {
+    private int id;
+    private String name;
+    private String passwd;
+    private int enable;
+
+    @Id
+    @Column(name = "id", nullable = false)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "name", nullable = false, length = 255)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "passwd", nullable = false, length = 255)
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+
+    @Basic
+    @Column(name = "enable", nullable = false)
+    public int getEnable() {
+        return enable;
+    }
+
+    public void setEnable(int enable) {
+        this.enable = enable;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserEntity that = (UserEntity) o;
+
+        if (id != that.id) return false;
+        if (enable != that.enable) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        if (passwd != null ? !passwd.equals(that.passwd) : that.passwd != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (passwd != null ? passwd.hashCode() : 0);
+        result = 31 * result + enable;
+        return result;
+    }
+}
